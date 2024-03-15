4cab36c6a71c4628a2f73adf3dbf13d666d657c1
abdobassel <abdobassel@gmail.com>
2024-03-15 06:03:22 +0200

    finish add note and validate

--- apis/add_note.php
+++ apis/add_note.php
@@ -1,1 +1,23 @@
 <?php
+include '../init.php';
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    if (isset($_POST['user_id'])) {
+        $userid = filter_input(INPUT_POST, 'user_id', FILTER_VALIDATE_INT);
+    }
+    $body = filter_var($_POST['body'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $title = htmlspecialchars(strip_tags($_POST['title']));
+
+    $stmt = $con->prepare('INSERT INTO notes (title, body, user_id) VALUES (?, ?, ?)');
+    $stmt->execute(array($title, $body, $userid));
+
+    if ($stmt->rowCount() > 0) {
+
+
+        echo json_encode(array('status' => true, 'msg' => 'done'));
+        http_response_code(200);
+    } else {
+        echo json_encode(array('status' => false, 'msg' => 'Not add'));
+        http_response_code(404);
+    }
+}
