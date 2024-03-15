0254f3c236a26a9032b9019e2747752253d6e48d
abdobassel <abdobassel@gmail.com>
2024-03-15 21:28:16 +0200

    Update Note Api Done

--- apis/update_note.php
+++ apis/update_note.php
@@ -1,2 +1,36 @@
 <?php
-include '../config.php';
+
+
+include '../init.php';
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    if (isset($_POST['note_id'])) {
+        $noteid = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
+    }
+    $body = filter_var($_POST['body'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $title = htmlspecialchars(strip_tags($_POST['title']));
+
+    $checkNote = checkItem('id', 'notes', $noteid);
+
+    if ($checkNote > 0 && !empty($body) && !empty($title)) {
+
+        $stmt = $con->prepare("UPDATE notes SET title = ?,body = ? WHERE id = ?");
+        $stmt->execute(array($title, $body, $noteid));
+
+        if ($stmt->rowCount() > 0) {
+
+
+            echo json_encode(array('status' => true, 'msg' => 'Note Updated'));
+            http_response_code(200);
+        } else {
+            echo json_encode(array('status' => false, 'msg' => 'Failed Update'));
+            http_response_code(400);
+        }
+    } else {
+        echo json_encode(array('status' => false, 'msg' => 'No Such ID'));
+        http_response_code(404);
+    }
+} else {
+    echo json_encode(array('status' => false, 'msg' => 'Bad Request'));
+    http_response_code(400);
+}
