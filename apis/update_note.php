51d12807472d142b81bcc7dbb3a722b09a94b9c7
abdobassel <abdobassel@gmail.com>
2024-03-17 05:39:14 +0200

    refactor update

--- apis/update_note.php
+++ apis/update_note.php
@@ -1,36 +1,36 @@
 <?php
-
-
 include '../init.php';
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if (isset($_POST['note_id'])) {
         $noteid = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
     }
-    $body = filter_var($_POST['body'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $body = htmlspecialchars(strip_tags($_POST['body']));
     $title = htmlspecialchars(strip_tags($_POST['title']));
 
-    $checkNote = checkItem('id', 'notes', $noteid);
+    // التحقق مما إذا كان الـ note_id موجود والبيانات غير فارغة
+    if ($noteid && !empty($body) && !empty($title)) {
+        // تحديث البيانات في قاعدة البيانات
+        $stmt = $con->prepare("UPDATE notes SET title = ?, body = ? WHERE id = ?");
+        $stmt->execute([$title, $body, $noteid]);
 
-    if ($checkNote > 0 && !empty($body) && !empty($title)) {
-
-        $stmt = $con->prepare("UPDATE notes SET title = ?,body = ? WHERE id = ?");
-        $stmt->execute(array($title, $body, $noteid));
-
+        // التحقق مما إذا كان هناك صفوف تم تحديثها
         if ($stmt->rowCount() > 0) {
-
-
+            // إذا تم تحديث البيانات بنجاح
             echo json_encode(array('status' => true, 'msg' => 'Note Updated'));
             http_response_code(200);
         } else {
+            // إذا لم يتم تحديث أي بيانات
             echo json_encode(array('status' => false, 'msg' => 'Failed Update'));
             http_response_code(400);
         }
     } else {
-        echo json_encode(array('status' => false, 'msg' => 'No Such ID'));
-        http_response_code(404);
+        // إذا كان أحد البيانات غير متوفرة
+        echo json_encode(array('status' => false, 'msg' => 'Invalid Data'));
+        http_response_code(400);
     }
 } else {
+    // إذا كان الطلب غير صالح
     echo json_encode(array('status' => false, 'msg' => 'Bad Request'));
     http_response_code(400);
 }
